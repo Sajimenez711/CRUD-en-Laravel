e2404c3363e88656dcd009a3b3a24f34684e9b94
Unknown <sergio.jimenez@uabc.edu.mx>
2018-03-19 18:15:24 -0700

    2do video

--- public/js/app.js
+++ public/js/app.js
@@ -12548,20 +12548,8 @@ return /******/ (function(modules) { // webpackBootstrap
 });
 ;
 //# sourceMappingURL=axios.map
-var urlUsers = 'https://jsonplaceholder.typicode.com/users';
+
 		new Vue({
 			el: '#main',
-			created: function() {
-				this.getUsers();
-			},
-			data: {
-				lists: []
-			},
-			methods: {
-				getUsers: function() {
-					axios.get(urlUsers).then(response => {
-						this.lists = response.data
-					});
-				}
-			}
+		
 		});
