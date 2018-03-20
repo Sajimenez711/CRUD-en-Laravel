317c9d68d325d23256a9e8369977210e60bc6d0b
Unknown <sergio.jimenez@uabc.edu.mx>
2018-03-19 20:14:19 -0700

    4to video

--- public/js/app.js
+++ public/js/app.js
@@ -12549,7 +12549,22 @@ return /******/ (function(modules) { // webpackBootstrap
 ;
 //# sourceMappingURL=axios.map
 
-		new Vue({
-			el: '#main',
+		new Vue ({
+			el: '#crud',
+			created: function()
+			{
+				this.getKeeps();
+			},
+			data:{
+				keeps:[]
+			},
+			methods:{
+				getKeeps: function()
+				{
+					var urlKeeps = 'tasks';
+					axios.get(urlKeeps).then(response=>{this.keeps=response.data});
+					
+				}
+			}
 		
 		});
